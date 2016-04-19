6642bf5f09a26ea8f9fd880fe9028898a90daf38
Tom Neary <mail@opcontech.com>
2016-04-19 14:07:01 -0700

    Initial Commit

---
+++ LearnToLayout/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  LearnToLayout
+//
+//  Created by Thomas Neary on 4/19/16.
+//  Copyright © 2016 OpCon Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
